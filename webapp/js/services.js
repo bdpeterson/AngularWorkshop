8c976b43498f8b7123c0470c1db5ca5a35f4d91d
Brian Peterson <bpeterson@nvisia.com>
2014-02-03 09:16:24 -0600

    Updates to content and support Jasmine 1.3.x and 2.0.x.

--- webapp/js/services.js
+++ webapp/js/services.js
@@ -7,7 +7,7 @@ var restaurantServices = angular.module('restaurantServices', ['ngResource']);
 restaurantServices.factory('Restaurant', ['$resource',
     function ($resource) {
         return $resource('/restaurants/:restaurantId/:reservations', {restaurantId: '@restaurantId'}, {
-            GetRestaurant: {method: 'GET', params: {restaurantId: 'restaurantId'},cache:true},
+            GetRestaurant: {method: 'GET', params: {restaurantId: 'restaurantId'}, cache: true},
             GetAllRestaurants: {method: 'GET', params: {}, isArray: true},
             GetReservations: {method: 'GET', params: {restaurantId: 'restaurantId', reservations: 'reservations'}}
         });
@@ -17,4 +17,4 @@ restaurantServices.factory('Reservation', ['$resource',
     function ($resource) {
         return $resource('/reservations/:reservationId', {reservationId: '@reservationId'});
     }
-])
+]);
